259689613418d981b0bdd8594bce1fc8829ac43c
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-08-30 18:11:16 +0200

    try ta add some logic to on close

--- example/src/App.js
+++ example/src/App.js
@@ -2,13 +2,14 @@ import React, {useState} from 'react'
 import { CustomModalComponent } from 'custom-modal-library'
 import 'custom-modal-library/dist/index.css'
 
+
 const App = () => {
   const [modal, setModal] = useState(false);
-  const Toggle = () => setModal(!modal);
+  const OpenModal = () => setModal(!modal);
 
   return (
     <>
-      <button onClick={()=> { Toggle()}}>Open Me</button>
+      <button onClick={()=> { OpenModal()}}>Open Me</button>
       <CustomModalComponent isOpen={modal}  fade={modal} customWidth={700} customHeight={100} color={'blue'} text='CustomModalComponent Example 😄' />
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import styles from './styles.module.css'
 
-export const CustomModalComponent = ({ text, color, customWidth, customHeight, isOpen, fade }) => {
-  console.log(color, customWidth, customHeight)
-  const closeModal = () => {
-    console.log("courcou")
+export function CustomModalComponent ({ text, color, customWidth, customHeight, isOpen, fade }) {
+  console.log(color, customWidth, customHeight);
+
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    // 👇️ use document.getElementById()
+    const el = document.getElementById('containerModal');
+    console.log(el);
+
+    // 👇️ (better) use a ref
+    const el2 = ref.current;
+    console.log(el2);
+  }, []);
+  // function closeModal() {
+  //   console.log("Close Modal", 'fade', fade, 'isOpen', isOpen);
+  // isOpen = false;
+  // fade = false;
+  // }
+
+  function closeModal() {
+    console.log('wxb6=source.org',  ref.current)
+    ref.current.style = 'display: none;';
+
   }
 
 
+
   return isOpen ? (
+    <div ref={ref} id='containerModal'>
     <div className={styles.modal + " "+ (fade ? styles.modalFade : '')}
     style={{width:customWidth,
      height:customHeight}}>
-      <div className={ styles.modalOverlay }  onClick={closeModal()} />
-      <span role="button" className={ styles.modalClose} aria-label="close" onClick={closeModal()}>
+      <div className={ styles.modalOverlay }  onClick={() => {closeModal()}} />
+      <span role="button" className={ styles.modalClose} aria-label="close" onClick={() =>{closeModal()}}>
         x
       </span>
       <div className={ styles.modalBody}> {text}</div>
+    </div>
     </div>
   ) : null
 }
